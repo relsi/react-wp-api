53b5ce70c3df0a66e9a720d8680a87f579d7511a
Relsi Maron <relsi@codeup.com.br>
2021-01-14 11:57:56 -0300

    feat: add getToken endpint

---
+++ src/api/endpoints/getToken.js
@@ -0,0 +1,16 @@
+import { API_URL } from '../api';
+
+function getToken(body) {
+  return {
+    url: API_URL + '/jwt-auth/v1/token',
+    options: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+export default getToken;
--- src/pages/auth/LoginForm.js
+++ src/pages/auth/LoginForm.js
@@ -3,29 +3,24 @@ import { Link } from 'react-router-dom';
 import Input from '../../components/form/Input';
 import Button from '../../components/form/Button';
 import useForm from '../../hooks/useForm';
+import getToken from '../../api/endpoints/getToken';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validateField() && password.validateField()) {
-      fetch('http://localhost:10003/api/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(),
-      })
-        .then((response) => {
-          console.log(response);
-          return response.json();
-        })
-        .then((json) => {
-          console.log(json);
-        });
+      const { url, options } = getToken({
+        username: username.value,
+        password: password.value,
+      });
+
+      const response = await fetch(url, options);
+      const json = await response.json();
+      window.localStorage.setItem('token', json.token);
     }
   }
 
